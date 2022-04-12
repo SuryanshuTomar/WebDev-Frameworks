be7598e545c7e7a0203d014496dc04c580870caa
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-04-12 19:41:27 +0530

    Adding Modal UI Component

---
+++ React JS/Practice/PracticeFoodApp/src/components/UI/Modal.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+	return <div className={Classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+	return (
+		<div className={Classes.modal}>
+			<div className={Classes.content}>{props.children}</div>
+		</div>
+	);
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+	return (
+		<React.Fragment>
+			{ReactDOM.createPortal(<Backdrop />, portalElement)}
+			{ReactDOM.createPortal(
+				<ModalOverlay>{props.children}</ModalOverlay>,
+				portalElement
+			)}
+		</React.Fragment>
+	);
+};
+
+export default Modal;
