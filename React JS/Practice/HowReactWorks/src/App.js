bef702337c3a821d7b39cf62489f9d908c36795c
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-04-17 11:31:48 +0530

    Used React.memo for practice

--- React JS/Practice/HowReactWorks/src/App.js
+++ React JS/Practice/HowReactWorks/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+	console.log("APP EXECUTED !!");
+
+	const [showPara, setShowPara] = useState(false);
+	const toggleParaHandler = () => {
+		setShowPara((showPara) => !showPara);
+	};
+	return (
+		<div className="app">
+			<h1>Hi there!</h1>
+			{/* It doesn't matter if the state, props or context changes in the child component if a parent component is re-evaluated then all of its child components will be re-evaluated too. */}
+			<DemoOutput show={false} />
+			<Button onClick={toggleParaHandler}>Toggle Paragraph !</Button>
+		</div>
+	);
 }
 
 export default App;
