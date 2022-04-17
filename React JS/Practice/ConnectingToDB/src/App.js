3108e2a0943c45d004694e2419a9a8b4dc3d51df
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-04-18 01:05:24 +0530

    Fetching Data and Conditionally Rendering Data

--- React JS/Practice/ConnectingToDB/src/App.js
+++ React JS/Practice/ConnectingToDB/src/App.js
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	async function fetchMoviesHandler() {
+		setIsLoading(true);
+		const response = await fetch("http://swapi.dev/api/films");
+		const data = await response.json();
+		const trasformedMovies = data.results.map((movieData) => {
+			return {
+				id: movieData.episode_id,
+				title: movieData.title,
+				openingText: movieData.opening_crawl,
+				releaseData: movieData.release_date,
+			};
+		});
+		setMovies(trasformedMovies);
+		setIsLoading(false);
+	}
+
+	return (
+		<React.Fragment>
+			<section>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+				{!isLoading && movies.length === 0 && <p>Found No Movies !</p>}
+				{isLoading && <p>Fetching Movies....</p>}
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
