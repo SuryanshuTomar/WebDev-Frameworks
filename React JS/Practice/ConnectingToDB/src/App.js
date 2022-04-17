08fc6493b9ecfeea5d51dbaca607c4769fa0d60d
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-04-18 01:54:23 +0530

    Adding HTTP Post to add movies

--- React JS/Practice/ConnectingToDB/src/App.js
+++ React JS/Practice/ConnectingToDB/src/App.js
@@ -13,22 +13,36 @@ function App() {
 		setIsLoading(true);
 		setError(null);
 		try {
-			const response = await fetch("https://swapi.dev/api/films/");
+			const response = await fetch(
+				"https://react-http-20964-default-rtdb.firebaseio.com/movies.json"
+			);
 			if (!response.ok) {
 				throw new Error("Something went wrong!");
 			}
 
 			const data = await response.json();
 
-			const transformedMovies = data.results.map((movieData) => {
-				return {
-					id: movieData.episode_id,
-					title: movieData.title,
-					openingText: movieData.opening_crawl,
-					releaseDate: movieData.release_date,
-				};
-			});
-			setMovies(transformedMovies);
+			const loadedMovies = [];
+
+			for (const key in data) {
+				loadedMovies.push({
+					id: key,
+					title: data[key].title,
+					releaseDate: data[key].releaseDate,
+					openingText: data[key].openingText,
+				});
+			}
+
+			// const transformedMovies = data.results.map((movieData) => {
+			// 	return {
+			// 		id: movieData.episode_id,
+			// 		title: movieData.title,
+			// 		openingText: movieData.opening_crawl,
+			// 		releaseDate: movieData.release_date,
+			// 	};
+			// });
+			// setMovies(transformedMovies);
+			setMovies(loadedMovies);
 		} catch (error) {
 			setError(error.message);
 		}
@@ -39,8 +53,23 @@ function App() {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
-	function addMovieHandler(movie) {
-		console.log(movie);
+	async function addMovieHandler(movie) {
+		try {
+			const response = await fetch(
+				"https://react-http-20964-default-rtdb.firebaseio.com/movies.json",
+				{
+					method: "POST",
+					body: JSON.stringify(movie),
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			const data = await response.json();
+			console.log(data);
+		} catch (err) {
+			setError(err.message);
+		}
 	}
 
 	let content = <p>Found no movies.</p>;
