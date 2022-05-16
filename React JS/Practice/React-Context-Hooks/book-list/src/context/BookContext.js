4b9ad3214c56351b6bdb650b294c3fbdf4a1a439
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-16 19:27:06 +0530

    BookList Persisting data feature added

--- React JS/Practice/React-Context-Hooks/book-list/src/context/BookContext.js
+++ React JS/Practice/React-Context-Hooks/book-list/src/context/BookContext.js
@@ -1,15 +1,26 @@
-import React, { useState, createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { BookReducer } from "../reducers/BookReducer";
 import { v4 as uuid } from "uuid";
 
 export const BookContext = createContext();
 
 function BookContextProvider(props) {
-	const [bookList, dispatchBook] = useReducer(BookReducer, [
-		{ title: "The Bhagvad Gita", author: "Lord Krishna", id: uuid() },
-		{ title: "The Final Empire", author: "Brandon Sanderson", id: uuid() },
-		{ title: "Name of the Wind", author: "Patrick Rothfuss", id: uuid() },
-	]);
+	const [bookList, dispatchBook] = useReducer(
+		BookReducer,
+		[
+			{ title: "The Bhagvad Gita", author: "Lord Krishna", id: uuid() },
+			{ title: "The Final Empire", author: "Brandon Sanderson", id: uuid() },
+			{ title: "Name of the Wind", author: "Patrick Rothfuss", id: uuid() },
+		],
+		() => {
+			const localData = JSON.parse(localStorage.getItem("books"));
+			return localData ? localData : [];
+		}
+	);
+
+	useEffect(() => {
+		localStorage.setItem("books", JSON.stringify(bookList));
+	}, [bookList]);
 
 	return (
 		<BookContext.Provider value={{ bookList, dispatchBook }}>
