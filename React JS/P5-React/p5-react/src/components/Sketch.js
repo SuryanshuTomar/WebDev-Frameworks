bb44ed1e754aa566c8bc8a9db163a6da5634b3f1
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-17 23:25:08 +0530

    p5 sketch in react complete

--- React JS/P5-React/p5-react/src/components/Sketch.js
+++ React JS/P5-React/p5-react/src/components/Sketch.js
@@ -3,6 +3,7 @@ import { ReactP5Wrapper } from "react-p5-wrapper";
 
 function Sketch() {
 	const [rotation, setRotation] = useState(0);
+	const [speed, setSpeed] = useState(0);
 
 	useEffect(() => {
 		const interval = setInterval(
@@ -15,10 +16,35 @@ function Sketch() {
 		};
 	}, []);
 
+	const speedHandler = (currentRotaionSpeed) => {
+		setSpeed(currentRotaionSpeed);
+	};
+
 	return (
 		<>
 			{/* {console.log("Rendered")} */}
-			<ReactP5Wrapper sketch={sketch} rotation={rotation} />
+			<ReactP5Wrapper
+				sketch={sketch}
+				rotation={rotation}
+				getSpeed={speedHandler}
+			/>
+			<div
+				style={{
+					height: "30px",
+					width: "350px",
+					position: "absolute",
+					background: "grey",
+					paddingTop: "5px",
+					top: "10px",
+					left: "20px",
+					color: "white",
+					textAlign: "center",
+					zIndex: "1",
+					borderRadius: "25px",
+				}}
+			>
+				Speed: {speed}
+			</div>
 		</>
 	);
 }
@@ -37,20 +63,25 @@ function sketch(p5) {
 		if (props.rotation) {
 			rotation = (props.rotation * Math.PI) / 180;
 		}
+		speed(props);
 	};
 
 	p5.windowResized = () => {
 		p5.resizeCanvas(p5.windowWidth, p5.windowHeight, p5.WEBGL);
 	};
 
+	function speed(props) {
+		props.getSpeed(rotation);
+	}
+
 	p5.draw = () => {
-		console.log(rotation);
+		// console.log(rotation);
 		p5.background(100);
 		p5.normalMaterial();
 		p5.noStroke();
 		p5.push();
-		// p5.rotateY(rotation);
-		p5.rotateX(rotation);
+		p5.rotateY(-rotation);
+		// p5.rotateX(rotation);
 		p5.box(100);
 		p5.pop();
 	};
