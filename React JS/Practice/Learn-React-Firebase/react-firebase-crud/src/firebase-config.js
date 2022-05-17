1779dc3ee769493615fa3065a075d89a5d9067dc
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-17 11:23:44 +0530

    Firebase crud complete

--- React JS/Practice/Learn-React-Firebase/react-firebase-crud/src/firebase-config.js
+++ React JS/Practice/Learn-React-Firebase/react-firebase-crud/src/firebase-config.js
@@ -1,21 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFireStore } from "@firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyCcHsxnYSkiVDhqNSdmwBphTDglk7Tf3ZY",
-	authDomain: "fir-react-crud-cbad4.firebaseapp.com",
-	projectId: "fir-react-crud-cbad4",
-	storageBucket: "fir-react-crud-cbad4.appspot.com",
-	messagingSenderId: "685339670597",
-	appId: "1:685339670597:web:2b9f7ba4bac365f7700b2c",
+	apiKey: "AIzaSyBRwOVxoeiZCikiP5Q96CYjjTbrXGpxmko",
+	authDomain: "fir-react-crud-27a1d.firebaseapp.com",
+	projectId: "fir-react-crud-27a1d",
+	storageBucket: "fir-react-crud-27a1d.appspot.com",
+	messagingSenderId: "2967840211",
+	appId: "1:2967840211:web:ea6f3599ab1394b75c7b26",
+	measurementId: "G-EZ6X44YLY1",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFireStore(app);
+export const db = getFirestore(app);
