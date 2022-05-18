6acb856d5493383cd30968059a25dd115555a543
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-18 22:51:56 +0530

    starfield completed

--- P5/CodingTrainChallenges/Challenge-01/sketch.js
+++ P5/CodingTrainChallenges/Challenge-01/sketch.js
@@ -1,7 +1,24 @@
+let stars = new Array(800);
+
+let sp;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	for (let i = 0; i < stars.length; i++) {
+		stars[i] = new Star();
+	}
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+	sp = map(mouseX, 0, width, 5, 40);
 	background(0);
+	translate(width / 2, height / 2);
+	for (let i = 0; i < stars.length; i++) {
+		stars[i].update(sp);
+		stars[i].show();
+	}
 }
