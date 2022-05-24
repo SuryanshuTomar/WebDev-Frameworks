4d2898ee778c250882ca7b73e796747924f60004
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-24 19:14:17 +0530

    Files changes

--- React JS/Practice/HowReactWorks-P1/src/components/Demo/MyParagraph.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const MyParagraph = (props) => {
-	console.log("MyParagraph RUNNING !!");
-	return <p>{props.chidlren}</p>;
-};
-
-export default MyParagraph;
--- React JS/Practice/PracticeFoodApp/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Classes from "./Card.module.css";
-
-const Card = (props) => {
-	return <div className={Classes.card}>{props.children}</div>;
-};
-
-export default Card;
