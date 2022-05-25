421b066dd521468cee19c572d63e8b8083079d73
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-26 03:14:27 +0530

    Redux Core Concepts and Principles

---
+++ React JS/Notes/ReduxNotes.js
@@ -0,0 +1,14 @@
+// What is Redux ?
+// - Redux is a Predictable State Container for JS Apps
+// - It helps you write applications that behave consistently, run in different environments (client, server, and native), and are easy to test.
+// - We can use Redux together with React, or with any other view library. It is tiny (2kB, including dependencies), but has a large ecosystem of addons available.
+
+// - Three Core Concepts of Redux -
+// 1. Store - It holds the state of our application. i.e - Cake Shop.
+// 2. Action - It describes what will happen in the application. i.e - Cake Ordering.
+// 3. Reducer - Reducer is what ties the store and action together. It handles action and decide how to update the state. i.e - Shopkeeper.
+
+// - Three Core Principles of Redux -
+// 1. First Principle - "The global state of your application is stored as an object inside a single store." This means in redux, we have maintain our application state in a single object which would be managed by the Redux Store.
+// 2. Second Principle - "The only way to change the state is to dispatch an action, an object that describes what will happen". This means, to update the state of our application, we need to let Redux know about that with an action and we are not allowed to directly update the state object.
+// 3. Third Principle - "To specify how the state tree is updated based on actions, we have to write pure reducers". Pure Reducers are basically functions that takes (previous/currentState, action) as parameters and returns the newState.
