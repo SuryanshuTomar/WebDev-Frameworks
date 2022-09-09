9f167e0e695f74cb8036585b487ca27b702e5614
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-10 01:33:02 +0530

    Routes logic for home and about

--- React JS/ReactRouter-CodeEvolution/react-router/src/App.js
+++ React JS/ReactRouter-CodeEvolution/react-router/src/App.js
@@ -1,7 +1,16 @@
-
+import About from "./components/About";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="about" element={<About />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
