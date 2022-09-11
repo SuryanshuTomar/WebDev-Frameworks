2dc28a6b05eea59700b1d562eed5ffdb8e9af28b
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-11 17:35:04 +0530

    defined home, navbar and about

--- React JS/ReactRouter-CodeEvolution/react-router/src/App.js
+++ React JS/ReactRouter-CodeEvolution/react-router/src/App.js
@@ -1,10 +1,12 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="about" element={<About />} />
