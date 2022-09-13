664c7bc33cf16a0cb275c5164279069de5eaf344
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-13 19:24:39 +0530

    Attaching ref to classes

--- React JS/CodeEvolution/code-evolution-class/src/components/Refs/RefAttachedToClassComp.js
+++ React JS/CodeEvolution/code-evolution-class/src/components/Refs/RefAttachedToClassComp.js
@@ -9,14 +9,16 @@ export class RefAttachedToClassComp extends Component {
 	}
 
 	onClickHandler = () => {
-		// Calling the child component Method using "ref"
+		// Calling the child component Method using "componentRef"
+		// This "componentRef" is now pointing to the child class component
+		// So we can call child class methods using this "componentRef"
 		this.componentRef.current.focusInput();
 	};
 	render() {
 		return (
 			<div>
 				<h1>RefAttachedToClassComp</h1>
-				{/* Attaching ref to the class component*/}
+				{/* Attaching ref to the child class component*/}
 				<InputComp ref={this.componentRef} />
 				<button onClick={this.onClickHandler}>Focus Input</button>
 			</div>
