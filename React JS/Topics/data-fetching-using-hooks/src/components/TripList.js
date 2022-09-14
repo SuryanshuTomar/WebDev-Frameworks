bbf77c290013463a93efc58c000e22204da682b5
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-14 22:33:59 +0530

    Added Filters button and button styles

--- React JS/Topics/data-fetching-using-hooks/src/components/TripList.js
+++ React JS/Topics/data-fetching-using-hooks/src/components/TripList.js
@@ -3,22 +3,39 @@ import "./TripList.css";
 
 function TripList() {
 	const [trips, setTrips] = useState([]);
+	const [url, setUrl] = useState("http://localhost:3000/trips");
 
 	useEffect(() => {
-		fetch("http://localhost:3000/trips")
+		fetch(url)
 			.then((response) => response.json())
 			.then((data) => setTrips(data))
 			.catch((err) => console.log(err));
-	}, [trips]);
+	}, [url]);
 
 	return (
 		<div className="trip-list">
 			<h1>TRIP LIST</h1>
 			<hr />
+			<div className="filters">
+				<button
+					onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+				>
+					European Trips
+				</button>
+				<button
+					onClick={() => setUrl("http://localhost:3000/trips?loc=america")}
+				>
+					American Trips
+				</button>
+				<button onClick={() => setUrl("http://localhost:3000/trips")}>
+					All Trips
+				</button>
+			</div>
+			<hr />
 			<ul>
 				{trips.map((trip) => {
 					return (
-						<li key={trip.title}>
+						<li key={trip.id}>
 							<h3>Name: {trip.title}</h3>
 							<p>Price: {trip.price}</p>
 						</li>
