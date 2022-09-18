f80f1db70fe9dad11dbe51de8efadb39f5287e99
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-18 12:01:20 +0530

    Portal

--- React JS/CodeEvolution/code-evolution-class/src/components/Portals/Portal.js
+++ React JS/CodeEvolution/code-evolution-class/src/components/Portals/Portal.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-// Portal are mainly used for Models, tool-tips and pop-ups
-function Portal() {
-	return ReactDOM.createPortal(
-		<h1>Portal</h1>,
-		document.getElementById("portal-root")
-	);
+export class Portal extends Component {
+	render() {
+		// Portal are mainly used for Models, tool-tips and pop-ups
+		return ReactDOM.createPortal(
+			<h1>Portal</h1>,
+			document.getElementById("portal-root")
+		);
+	}
 }
 
 export default Portal;
