292cc3d8699fd6a68532e0741854e5961d347d77
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-18 12:42:33 +0530

    HoverCounter logic from HOC

---
+++ React JS/CodeEvolution/code-evolution-class/src/components/HigherOrderComponents/HoverCounter.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import UpdatedComponent from "./withCounter";
+
+export class HoverCounter extends Component {
+	render() {
+		return (
+			<div>
+				<h1>HoverCounter</h1>
+				<h3 onMouseOver={this.props.incrementCount}>
+					{this.props.name} Hovered {this.props.count} times
+				</h3>
+			</div>
+		);
+	}
+}
+
+export default UpdatedComponent(HoverCounter);
