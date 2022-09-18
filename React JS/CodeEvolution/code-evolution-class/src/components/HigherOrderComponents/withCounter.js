f16a686e6bdbd432b782a05f12770d06df9285b9
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-18 13:53:38 +0530

    Passing another arguments to HOC withCounter

--- React JS/CodeEvolution/code-evolution-class/src/components/HigherOrderComponents/withCounter.js
+++ React JS/CodeEvolution/code-evolution-class/src/components/HigherOrderComponents/withCounter.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // The HOC will be named starting with "with" followed by functionality in camelCase
-const withCounter = (WrappedComponent) => {
+const withCounter = (WrappedComponent, incrementNum = 1) => {
 	// The new enhanced component which will be returned will be named same as the HOC but with PascalCase
 	class WithCounter extends React.Component {
 		constructor(props) {
@@ -13,7 +13,9 @@ const withCounter = (WrappedComponent) => {
 		}
 
 		incrementCount = () => {
-			this.setState((prevState) => ({ count: prevState.count + 1 }));
+			this.setState((prevState) => ({
+				count: prevState.count + incrementNum,
+			}));
 		};
 
 		render() {
