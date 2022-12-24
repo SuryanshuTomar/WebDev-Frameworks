7db452101832e1ae7960eca5bfb723c977015df4
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-24 18:10:06 +0530

    Added Routing intro notes

---
+++ NextJs/Notes.js
@@ -0,0 +1,22 @@
+// => NEXT JS INTRO AND BASICS - 
+
+// - NEXT 12 and Before - 
+
+
+
+
+
+// -----------------------------------------------------------------------------------------------------
+// => ROUTING SECTION -
+
+// 1. Routing in a React APP - 
+//    I. Install a thrid party package
+//    II. routes.js file to configure the routes.
+//    III. For each route, create a component file, export the component, import it in routes.js and configure the new route with a path property.
+
+// 2. Routing in Next APP - 
+//    I. File-System based routing mechanism.
+//    II. When a file is added to pages folder in a project, it automatically becomes available as a route.
+//    III. By mixing and matching file names with a nested folder structure, it is possible to pretty much define the most common routing patterns.
+
+
