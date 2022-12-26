4a51cb7ec9394fe9c19974ce6746f8ed38c92b43
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-26 17:59:32 +0530

    Navigating programmatically

--- NextJs/basics/pages/index.js
+++ NextJs/basics/pages/index.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Homepage() {
+	const router = useRouter();
+
+	const handleClick = () => {
+		alert("Order Placed Succesfully !!");
+		router.push("/product");
+	};
+
 	return (
 		<div>
 			<h1>Route Page: Homepage or main index page</h1>
 			<Link href="/blog">Blog </Link> <br />
 			<Link href="/product">Product </Link>
+			<br />
+			<button onClick={handleClick}>Place Order</button>
 		</div>
 	);
 }
