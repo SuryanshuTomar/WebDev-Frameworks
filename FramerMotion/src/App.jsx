a8c00dbe213a3ea461a3385b6603f18480fbd8e6
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-27 12:47:31 +0530

    Added Boxes

--- FramerMotion/src/App.jsx
+++ FramerMotion/src/App.jsx
@@ -1,7 +1,25 @@
+import Box1 from "./components/Box1";
+import Box2 from "./components/Box2";
+import Box3 from "./components/Box3";
+import Box4 from "./components/Box4";
+
+// APP CSS File -
+import "./App.css";
+
 function App() {
 	return (
 		<div className="App">
-			<h1 >Framer Motion Animation</h1>
+			<hr />
+			<h1>Framer Motion Animation</h1>
+			<hr />
+			<Box1 />
+			<hr />
+			<Box2 />
+			<hr />
+			<Box3 />
+			<hr />
+			<Box4 />
+			<hr />
 		</div>
 	);
 }
