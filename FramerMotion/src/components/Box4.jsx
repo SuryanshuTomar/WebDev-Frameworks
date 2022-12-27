40da06551c5ca7f188ab3bba5806b154e06dee1b
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-27 12:47:57 +0530

    Box4 jsx

--- FramerMotion/src/components/Box4.jsx
+++ FramerMotion/src/components/Box4.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Box4() {
   return (
-    <div>Box4</div>
-  )
+		<div className="box-container">
+			<h3>BOX 4</h3>
+			<div className="box"></div>
+		</div>
+  );
 }
 
 export default Box4
