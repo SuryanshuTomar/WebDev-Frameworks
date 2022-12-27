510d258e5f468789c545603aaaad996aeaa98ccc
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-27 12:47:53 +0530

    Box3 jsx

--- FramerMotion/src/components/Box3.jsx
+++ FramerMotion/src/components/Box3.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Box3() {
   return (
-    <div>Box3</div>
-  )
+		<div className="box-container">
+			<h3>BOX 3</h3>
+			<div className="box"></div>
+		</div>
+  );
 }
 
 export default Box3
