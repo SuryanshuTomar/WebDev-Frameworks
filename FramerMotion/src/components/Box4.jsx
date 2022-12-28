f040800c139f70cfa5d22e98d75cc49a96082862
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-28 13:27:08 +0530

    Animating using keyframes

--- FramerMotion/src/components/Box4.jsx
+++ FramerMotion/src/components/Box4.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 function Box4() {
-  return (
+	return (
 		<div className="box-container">
 			<h3>BOX 4</h3>
-			<div className="box"></div>
+			<motion.div
+				className="box"
+				animate={{
+					// keyframes for every property
+					scale: [1, 1.4, 0.6, 1.2, 0.8, 1],
+					rotate: [360, 0, 180, 0, 360],
+					borderRadius: ["1%", "25%", "50%", "25%", "1%"],
+				}}
+				transition={{
+					duration: 3,
+					delay: 0.5,
+				}}
+			></motion.div>
 		</div>
-  );
+	);
 }
 
-export default Box4
+export default Box4;
