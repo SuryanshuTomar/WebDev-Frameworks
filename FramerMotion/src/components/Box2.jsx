dd3edfc4866bc9ab8e1d4f8cd0cb1f08bfdab3b3
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-28 12:12:20 +0530

    Box 2 Animation on Drag, hover and tap

--- FramerMotion/src/components/Box2.jsx
+++ FramerMotion/src/components/Box2.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Box2() {
 	return (
 		<div className="box-container">
 			<h3>BOX 2</h3>
-			<div className="box"></div>
+			<motion.div
+				className="box"
+				whileHover={{ scale: 1.1, boxShadow: "0 0 2px pink" }}
+				whileTap={{ scale: 0.9 }}
+				drag
+				dragConstraints={{ top: 10, bottom: 10, left: 10, right: 10 }}
+				// dragElastic={0.2}
+				// dragMomentum={false}
+				// dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }}
+			></motion.div>
 		</div>
 	);
 }
