2f6e13e578f1090603933346f66d8c0a70e6e272
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-28 13:13:06 +0530

    Animation using variant

--- FramerMotion/src/components/Box3.jsx
+++ FramerMotion/src/components/Box3.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 function Box3() {
-  return (
+	const boxVariant = {
+		initial: {
+			x: "-100vw",
+		},
+		final: {
+			x: 0,
+			transition: {
+				delay: 0.5,
+				when: "beforeChildren",
+				staggerChildren: 0.2,
+			},
+		},
+	};
+
+	const boxListVariant = {
+		initial: {
+			x: "50px",
+			opacity: 0,
+		},
+		final: {
+			x: 0,
+			opacity: 1,
+		},
+	};
+
+	const boxDotVariant = {
+		initial: {
+			opacity: 0,
+		},
+		final: {
+			opacity: 1,
+			transition: {
+				delay: 1,
+			},
+		},
+	};
+	return (
 		<div className="box-container">
 			<h3>BOX 3</h3>
-			<div className="box"></div>
+			<motion.div
+				className="box"
+				variants={boxVariant}
+				initial="initial"
+				animate="final"
+			>
+				{[1, 2, 3].map((boxItem) => (
+					<motion.li
+						className="box-item"
+						key={boxItem}
+						variants={boxListVariant}
+					>
+						<motion.div
+							className="box-dot"
+							variants={boxDotVariant}
+						></motion.div>
+					</motion.li>
+				))}
+			</motion.div>
 		</div>
-  );
+	);
 }
 
-export default Box3
+export default Box3;
