d71c66e69bdca186ca07e54857e64daa2596575d
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-20 13:07:15 +0530

    completed

--- TanStackQuery/react-query-l/src/components/Charachters.jsx
+++ TanStackQuery/react-query-l/src/components/Charachters.jsx
@@ -5,30 +5,76 @@ import RickMortyFetch from "../axios/RickMortyFetch";
 import SingleCharachter from "./SingleCharachter";
 
 function Charachters() {
-	const fetchCharachers = async () => {
-		const response = await RickMortyFetch("/character");
+	const [page, setPage] = useState(1);
 
+	const fetchCharachers = async (keys) => {
+		const { queryKey } = keys;
+		const response = await RickMortyFetch(`/character?page=${queryKey[1]}`);
 		return response.data;
 	};
 
-	const { data: charachtersData, status } = useQuery(
-		"charachters",
-		fetchCharachers
-	);
+	const {
+		data: charachtersData,
+		status,
+		isLoading,
+		isError,
+		isPreviousData,
+	} = useQuery(["charachters", page], fetchCharachers, {
+		keepPreviousData: true, // this will cache the previous data which will help us in replacing it will new data
+	});
 
-	if (status === "loading") {
-		return <div>Loading Data...</div>;
+	// Alternative to check if the data is loading or the fetch request throws error from react query
+	// if (status === "loading") {
+	// 	return <div>Loading Data...</div>;
+	// }
+	// if (status === "error") {
+	// 	return <div>Something Went Wrong!!!</div>;
+	// }
+
+	if (isLoading) {
+		return <h2 style={{ color: "white" }}>Loading Data...</h2>;
 	}
-	if (status === "error") {
-		return <div>Something Went Wrong!!!</div>;
+	if (isError) {
+		return <h2 style={{ color: "white" }}>Something Went Wrong!!!</h2>;
 	}
 
+	// console.log(charachtersData);
+	// console.log(page);
 	return (
-		<div className="characters">
-			{charachtersData.results.map((character) => (
-				<SingleCharachter key={character.id} character={character} />
-			))}
-		</div>
+		<>
+			<h2 style={{ color: "white" }}>Current Page : {page}</h2> <br />
+			<div className="characters">
+				{charachtersData.results.map((character) => (
+					<SingleCharachter key={character.id} character={character} />
+				))}
+			</div>
+			<div>
+				{/* Alternate buttons */}
+				{/* {charachtersData.info.prev && (
+					<button onClick={() => setPage((page) => page - 1)}>
+						Prev Page
+					</button>
+				)}
+				{charachtersData.info.next && (
+					<button onClick={() => setPage((page) => page + 1)}>
+						Next Page
+					</button>
+				)} */}
+
+				<button
+					disabled={isPreviousData || !charachtersData.info.prev}
+					onClick={() => setPage((page) => page - 1)}
+				>
+					Prev Page
+				</button>
+				<button
+					disabled={isPreviousData || !charachtersData.info.next}
+					onClick={() => setPage((page) => page + 1)}
+				>
+					Next Page
+				</button>
+			</div>
+		</>
 	);
 }
 
