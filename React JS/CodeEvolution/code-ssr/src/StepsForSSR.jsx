645ea8ad2b2ebcefce100ba62c9c0cb269cfe988
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-20 09:26:08 +0530

    changes for react 18

--- React JS/CodeEvolution/code-ssr/src/StepsForSSR.jsx
+++ React JS/CodeEvolution/code-ssr/src/StepsForSSR.jsx
@@ -5,8 +5,8 @@ import dist from "@vitejs/plugin-react";
 // github for step : https://github.com/anil-sidhu/react-ssr-with-routing
 
 // Some Changes in above github steps :
-// For Step1: In the main.jsx or index.jsx File, render the root element using the ReactDOM.hydrate() method instead of using the ReactDOM.render() method.
+// For Step1: In the main.jsx or index.jsx File, render the root element using the
+// ReactDOM.hydrateRoot() method instead of using the ReactDOM.render() method.
 // For Server Folder:
 //    1. File Extension should be cjs(common js) if you are using vite otherwise it will treated as an ESModule File.
 //    2. If you are using vite to build the react application then, also change file path that is serving the html page from build folder to dist (distribution) folder as vite builds the files in the dist folder
-
--- React JS/CodeEvolution/code-ssr/src/main.jsx
+++ React JS/CodeEvolution/code-ssr/src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// cLient side rendering
+// const root = ReactDOM.createRoot(rootElem);
 // root.render(
 // 	<React.StrictMode>
 // 		<App />
 // 	</React.StrictMode>
 // );
 
-root.hydrate(<App />);
+// server side rendering
+// Method 1:
+const rootElem = document.getElementById("root");
+// ReactDOM.hydrate(<App />, rootElem);
+
+// Method 2: Recommended from react 18
+ReactDOM.hydrateRoot(rootElem, <App />);
