6eab58ac33be9b5913b933e6930f9a7d6a50181b
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-22 17:21:41 +0530

    Immer example

--- Redux/Learn-Redux-CodeEvolution/index.js
+++ Redux/Learn-Redux-CodeEvolution/index.js
@@ -1,114 +1,69 @@
-// Import redux
 const redux = require("redux");
+const produce = require("immer").produce;
 
-// GET CREATESTORE INSTANCE
-const createStore = redux.createStore;
-const bindActionCreators = redux.bindActionCreators;
-
-// ACTION TYPE
-const CAKE_ORDERED = "CAKE_ORDERED";
-const RESTORE_CAKE = "RESTORE_CAKE";
-const ICECREAM_ORDERED = "ICECREAM_ORDERED";
-const ICECREAM_RESTOCK = "ICECREAM_RESTOCK";
-
-// ACTION CREATOR
-function orderCake(qty = 1) {
-	return {
-		type: CAKE_ORDERED,
-		payload: qty,
-	};
-}
-
-function restockCake(qty = 1) {
-	return {
-		type: RESTORE_CAKE,
-		payload: qty,
-	};
-}
-
-function orderIcecream(qty = 1) {
-	return {
-		type: ICECREAM_ORDERED,
-		payload: qty,
-	};
-}
-
-function restockIcecream(qty = 1) {
-	return {
-		type: ICECREAM_RESTOCK,
-		payload: qty,
-	};
-}
-
-// INITIAL STATE
 const initialState = {
-	noOfCakes: 10,
-	noOfIcecreams: 20,
+	name: "Death Slayer",
+	address: {
+		street: "123 Side St",
+		city: "New York City",
+		state: "NY",
+	},
 };
 
-// REDUCER FUNCTION
-const shopReducer = (state = initialState, action) => {
+const STREET_UPDATED = "STREET_UPDATED";
+const updateStreet = (street) => {
+	return {
+		type: STREET_UPDATED,
+		payload: street,
+	};
+};
+
+const reducer = (state = initialState, action) => {
 	switch (action.type) {
-		case CAKE_ORDERED:
-			return { ...state, noOfCakes: state.noOfCakes - action.payload };
-		case RESTORE_CAKE:
-			return { ...state, noOfCakes: state.noOfCakes + action.payload };
-		case ICECREAM_ORDERED:
-			return {
-				...state,
-				noOfIcecreams: state.noOfIcecreams - action.payload,
-			};
-		case ICECREAM_RESTOCK:
-			return {
-				...state,
-				noOfIcecreams: state.noOfIcecreams + action.payload,
-			};
-		default:
+		case STREET_UPDATED:
+			// 	Manual updating of the state in redux
+			// 	return {
+			// 		...state,
+			// 		address: {
+			// 			...state.address,
+			// 			street: action.payload,
+			// 		},
+			// };
+			// Updating state using immer.producer method
+			// Syntax ->
+			// produce(initialState, Function(immutableState -> using which we can update the state))
+			return produce(state, (draft) => {
+				draft.address.city = action.payload;
+			});
+		default: {
 			return state;
+		}
 	}
 };
 
-// CREATE STORE
-const store = createStore(shopReducer);
+// get the creatStore instance
+const createStore = redux.createStore;
 
-// GET INITIAL STATE
-console.log("Initial State : ", store.getState());
+// get the bindActionCreators instance
+const bindActionCreators = redux.bindActionCreators;
 
-// SUBSCRIBE TO THE STORE
-// GET UPDATED STATE FROM STORE
-const unsubscribe = store.subscribe(() =>
-	console.log("Updated state: ", store.getState())
-);
+// create the store
+const store = createStore(reducer);
 
-// DISPATCH ACTION METHOD - 1
-// store.dispatch(orderCake());
-// store.dispatch(orderCake());
-// store.dispatch(orderCake(5));
-// store.dispatch(restockCake());
-// store.dispatch(restockCake(2));
-// store.dispatch(restockCake(4));
+// Print the initial state
+console.log("Initial State: ", initialState);
 
-// DISPATCH ACTION METHOD - 2
-// 1. BIND ACTION CREATORS TO DISPATCH METHOD
-const actions = bindActionCreators(
-	{ orderCake, restockCake, orderIcecream, restockIcecream },
-	store.dispatch
-);
+// Subscribe to the store
+const unsubscribe = store.subscribe(() => {
+	console.log("Updated State: ", store.getState());
+});
 
-// 2. CALL THE ACTION CREATOR BOUND WITH DISPATCH METHOD USING ACTIONS
-actions.orderCake();
-actions.orderCake();
-actions.orderCake(5);
-actions.restockCake();
-actions.restockCake(2);
-actions.restockCake(4);
+// Dispatch Action Method 1
+// store.dispatch(updateStreet("456 Hyperloop Line"));
 
-actions.orderIcecream();
-actions.orderIcecream(4);
-actions.orderIcecream(12);
-actions.restockIcecream();
-actions.restockIcecream(4);
-actions.restockIcecream(10);
+// Dispatch Action Method 2
+const actionDispatcher = bindActionCreators({ updateStreet }, store.dispatch);
+actionDispatcher.updateStreet("465 Main St");
 
-// UNSUBSCRIBE TO THE STORE
+// Unsubscribe to the store
 unsubscribe();
