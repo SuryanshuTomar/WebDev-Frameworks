ebfae27206264038e5853cf0bdc80a74b63aa357
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-23 13:33:01 +0530

    Redux concerns and why redux toolkit.

---
+++ Redux/ReduxToolKitNotes.js
@@ -0,0 +1,24 @@
+// => REDUX CONCERNS -
+
+// - Redux requires too much boilerplate code : 
+//    1. Action 
+//    2. Action Object
+//    3. Action Creator
+//    4. Reducer Function 
+//    5. Action Creator Binder and Dispatcher
+
+// - A lot of other packages have to be installed to work with redux
+//    1. redux-thunk
+//    2. immer
+//    3. redux-devtools
+
+// - This is what resulted in the creation of Redux-Toolkit Library.
+
+// -----------------------------------------------------------------------------------------------------
+// => REDUX TOOLKIT - 
+
+// - Redux Toolkit is the official, opinionated, batteries-included toolset for the efficient redux development. It - 
+//    1. Provideds abstraction over the setup process
+//    2. Handles the most common use cases.
+//    3. Includes some of the useful utilties.
+
