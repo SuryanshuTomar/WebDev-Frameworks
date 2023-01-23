da91223215421274d2f14c3e22800d4f63d6587b
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-23 16:16:28 +0530

    dispatching action for icecream

--- Redux/Learn-ReduxToolkit-CodeEvolution/index.js
+++ Redux/Learn-ReduxToolkit-CodeEvolution/index.js
@@ -3,6 +3,7 @@ const store = require("./app/store");
 
 // IMPORT ACTION CREATORS FROM THE SLICE FILES
 const { cakeActions } = require("./features/cake/cakeSlice");
+const { icecreamActions } = require("./features/icecream/icecreamSlice");
 
 // PRINT THE INITIAL STATE
 console.log("Initial State: ", store.getState());
@@ -13,9 +14,14 @@ const unsubscribe = store.subscribe(() => {
 });
 
 // DISPATCH ACTION
-store.dispatch(cakeActions.cakeOrdered());
-store.dispatch(cakeActions.cakeOrdered(5));
-store.dispatch(cakeActions.cakeRestocked(5));
-store.dispatch(cakeActions.cakeRestocked(10));
+store.dispatch(cakeActions.cakeOrder());
+store.dispatch(cakeActions.cakeOrder(5));
+store.dispatch(cakeActions.cakeRestock());
+store.dispatch(cakeActions.cakeRestock(10));
+
+store.dispatch(icecreamActions.icecreamOrder());
+store.dispatch(icecreamActions.icecreamOrder(8));
+store.dispatch(icecreamActions.icecreamRestock());
+store.dispatch(icecreamActions.icecreamRestock(12));
 
 unsubscribe();
