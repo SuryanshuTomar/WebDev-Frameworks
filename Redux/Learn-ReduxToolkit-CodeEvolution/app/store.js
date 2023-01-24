1ea8cc502d07ecd8ced0a79607656c50bce8f736
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-24 19:51:48 +0530

    added User fetch Reducer and fetch user actions

--- Redux/Learn-ReduxToolkit-CodeEvolution/app/store.js
+++ Redux/Learn-ReduxToolkit-CodeEvolution/app/store.js
@@ -1,16 +1,18 @@
 // 1. IMPORT REDUXTOOLKIT PACKAGE AND GET AN INSTANCE OF CONFIGURESTORE FROM IT
 const configureStore = require("@reduxjs/toolkit").configureStore;
 
-const { getDefaultMiddleware } = require("@reduxjs/toolkit");
 // 2. IMPORT FEATURE'S/SLICE'S REDUCERS
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
 
-// IMPORT LOGGER AND GET A CREATELOGGER INSTANCE
+// 3. IMPORT LOGGER AND GET A CREATELOGGER INSTANCE
 const createLogger = require("redux-logger").createLogger;
 
-// CREATE A LOGGER
+// 4. CREATE A LOGGER
 const logger = createLogger();
+
+// 5. IMPORT THE USER REDUCER FOR THE ASYNC ACTION
+const userReducer = require("../features/user/userSlice");
 
 // 2. CREATE THE STORE
 // - To create a store we need to invoke configureStore() method from the redux-toolkit package.
@@ -18,18 +20,20 @@ const logger = createLogger();
 // - The reducer object combines all the slice/feature reducers we have created.
 // - Then we export the store so that it can be used in our application.
 
-// 3. APPLY MIDDLEWARE
+// 4. APPLY MIDDLEWARE
 // - The second key that configureStore() method take is the middleware key whose value is a function.
 // - In middleware function, has getDefaultMiddleware function passed down as parameter.
 // - And we have to return this getDefaultMiddleware function after concatinating our middleware function to it.
 
+// 6. COMBINE ALL THE REDUCER WHILE CONFIGURING THE STORE
 const store = configureStore({
 	reducer: {
 		cake: cakeReducer,
 		icecream: icecreamReducer,
+		user: userReducer,
 	},
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+	// middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
-// 3. EXPORT THE STORE
+// 7. EXPORT THE STORE
 module.exports = store;
--- Redux/Learn-ReduxToolkit-CodeEvolution/index.js
+++ Redux/Learn-ReduxToolkit-CodeEvolution/index.js
@@ -4,22 +4,28 @@ const store = require("./app/store");
 // IMPORT ACTION CREATORS FROM THE SLICE FILES
 const { cakeActions } = require("./features/cake/cakeSlice");
 const { icecreamActions } = require("./features/icecream/icecreamSlice");
+const { fetchUsersAction } = require("./features/user/userSlice");
 
 // PRINT THE INITIAL STATE
 console.log("Initial State: ", store.getState());
 
 // SUBSCRIBE TO THE STORE
-const unsubscribe = store.subscribe(() => {});
+const unsubscribe = store.subscribe(() => {
+	console.log("Updated State: ", store.getState());
+});
 
 // DISPATCH ACTION
-store.dispatch(cakeActions.cakeOrder());
-store.dispatch(cakeActions.cakeOrder(5));
-store.dispatch(cakeActions.cakeRestock());
-store.dispatch(cakeActions.cakeRestock(10));
+// store.dispatch(cakeActions.cakeOrder());
+// store.dispatch(cakeActions.cakeOrder(5));
+// store.dispatch(cakeActions.cakeRestock());
+// store.dispatch(cakeActions.cakeRestock(10));
 
-store.dispatch(icecreamActions.icecreamOrder());
-store.dispatch(icecreamActions.icecreamOrder(8));
-store.dispatch(icecreamActions.icecreamRestock());
-store.dispatch(icecreamActions.icecreamRestock(12));
+// store.dispatch(icecreamActions.icecreamOrder());
+// store.dispatch(icecreamActions.icecreamOrder(8));
+// store.dispatch(icecreamActions.icecreamRestock());
+// store.dispatch(icecreamActions.icecreamRestock(12));
 
-unsubscribe();
+// DISPATCH ASYNC ACTION
+store.dispatch(fetchUsersAction());
+
+// unsubscribe();
