2b09a960af3fabe1f56ab18346c29309efcd6103
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-25 19:17:24 +0530

    React hooks using TS

--- React JS/React-TS-DaveGray/src/App.tsx
+++ React JS/React-TS-DaveGray/src/App.tsx
@@ -4,18 +4,23 @@ import Counter from "./components/Counter";
 import { Heading } from "./components/Heading";
 import List from "./components/List";
 import { Section } from "./components/Section";
+import HooksMain from "./hooks/HooksMain";
 
 function App() {
 	const [count, setCount] = useState<number>(0);
 	return (
 		<div>
-			<Heading title={"React + Typescript"} />
+			{/* Basics */}
+			{/* <Heading title={"React + Typescript"} />
 			<Section title="Title for Section">This is my Section</Section>
 			<Counter setCount={setCount}>Count is : {count}</Counter>
 			<List
 				items={["☕ Coffee", "🌮 Tacos", "💻 Code"]}
 				render={(item: string) => <span className="gold">{item}</span>}
-			/>
+			/> */}
+
+			{/* Hooks */}
+			<HooksMain />
 		</div>
 	);
 }
---
+++ React JS/React-TS-DaveGray/src/hooks/HooksMain.tsx
@@ -0,0 +1,65 @@
+import {
+	useState,
+	useEffect,
+	useCallback,
+	useMemo,
+	useRef,
+	MouseEvent,
+	KeyboardEvent,
+} from "react";
+
+type User = {
+	id: number;
+	username: string;
+};
+
+type FibFunc = (num: number) => number;
+
+const getFibonnaci: FibFunc = (num) => {
+	if (num < 2) return num;
+	return getFibonnaci(num - 1) + getFibonnaci(num - 2);
+};
+
+const myNum: number = 37;
+
+// Component Starts Here
+const HooksMain = () => {
+	const [count, setCount] = useState<number>(0);
+	const [users, setUsers] = useState<User[] | null>(null);
+
+	// const inputRef = useRef<HTMLInputElement>(null!);
+	const inputRef = useRef<HTMLInputElement>(null);
+	console.log(inputRef?.current);
+	console.log(inputRef?.current?.value);
+
+	useEffect(() => {
+		console.log("Mounting");
+		console.log("Users: ", users);
+
+		return () => {
+			console.log("Unmounting");
+		};
+	}, [users]);
+
+	const addTwo = useCallback(
+		(
+			e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>
+		): void => setCount((prev) => prev + 1),
+		[]
+	);
+
+	const result = useMemo<number>((): number => getFibonnaci(myNum), [myNum]);
+
+	return (
+		<div>
+			<h1>{count}</h1>
+			<button onClick={addTwo}>Add</button>
+			<h4>
+				Fib of {myNum} is {result}
+			</h4>
+			<input type="text" ref={inputRef} />
+		</div>
+	);
+};
+
+export default HooksMain;
