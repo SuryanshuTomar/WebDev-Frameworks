3d661bb345dd7f15043699c68a9b085bc5a3fa78
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-25 11:12:03 +0530

    useDispatch hook

--- Redux/Learn-ReduxTooklit-React-CodeEvolution/src/features/cake/CakeView.jsx
+++ Redux/Learn-ReduxTooklit-React-CodeEvolution/src/features/cake/CakeView.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { cakeActions } from "./cakeSlice";
 
 function CakeView() {
 	// USESELECTOR HOOK TAKE A SELECTOR CALLBACK FUNCTION THAT RETURNS THE STATE OF ANY SLICE.
 	const noOfCakes = useSelector((state) => state.cake.noOfCakes);
+
+	// USEDISPATCH RETURNS A REFERENCE TO THE DISPATCH FUNCTION FROM THE STORE.
+	const dispatch = useDispatch();
 	return (
 		<div>
 			<h2>Number of cakes - {noOfCakes}</h2>
-			<button>Order cake</button>
-			<button>Restock cake</button>
+			<button onClick={() => dispatch(cakeActions.cakeOrder())}>
+				Order cake
+			</button>
+			<button onClick={() => dispatch(cakeActions.cakeRestock(5))}>
+				Restock cake
+			</button>
 		</div>
 	);
 }
--- Redux/Learn-ReduxTooklit-React-CodeEvolution/src/features/icecream/IcecreamVIew.jsx
+++ Redux/Learn-ReduxTooklit-React-CodeEvolution/src/features/icecream/IcecreamVIew.jsx
@@ -1,14 +1,37 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { icecreamActions } from "./icecreamSlice";
 
 function IcecreamVIew() {
+	const [restockValue, setRestockValue] = useState(1);
+
 	// USESELECTOR HOOK TAKE A SELECTOR CALLBACK FUNCTION THAT RETURNS THE STATE OF ANY SLICE.
 	const noOfIcecreams = useSelector((state) => state.icecream.noOfIcecreams);
+
+	// USEDISPATCH RETURNS A REFERENCE TO THE DISPATCH FUNCTION FROM THE STORE.
+	const dispatch = useDispatch();
 	return (
 		<div>
 			<h2>Number of icecreams - {noOfIcecreams}</h2>
-			<button>Order icecream</button>
-			<button>Restock icecream</button>
+			<br />
+			<label htmlFor="restock">Restock Value : </label>
+			<input
+				type="number"
+				id="restock"
+				value={restockValue}
+				onChange={(e) => setRestockValue(parseInt(e.target.value))}
+			/>{" "}
+			<br />
+			<button onClick={() => dispatch(icecreamActions.icecreamOrder())}>
+				Order icecream
+			</button>{" "}
+			<button
+				onClick={() =>
+					dispatch(icecreamActions.icecreamRestock(restockValue))
+				}
+			>
+				Restock icecream
+			</button>
 		</div>
 	);
 }
