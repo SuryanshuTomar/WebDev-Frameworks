907c25038b9a5a94a2badd53c55820c7efd121b4
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-25 10:51:47 +0530

    useSelector hook to access states from store.

--- Redux/Learn-ReduxTooklit-React-CodeEvolution/src/features/cake/CakeView.jsx
+++ Redux/Learn-ReduxTooklit-React-CodeEvolution/src/features/cake/CakeView.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function CakeView() {
+	// USESELECTOR HOOK TAKE A SELECTOR CALLBACK FUNCTION THAT RETURNS THE STATE OF ANY SLICE.
+	const noOfCakes = useSelector((state) => state.cake.noOfCakes);
 	return (
 		<div>
-			<h2>Number of cakes - </h2>
+			<h2>Number of cakes - {noOfCakes}</h2>
 			<button>Order cake</button>
 			<button>Restock cake</button>
 		</div>
--- Redux/Learn-ReduxTooklit-React-CodeEvolution/src/features/icecream/IcecreamVIew.jsx
+++ Redux/Learn-ReduxTooklit-React-CodeEvolution/src/features/icecream/IcecreamVIew.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function IcecreamVIew() {
+	// USESELECTOR HOOK TAKE A SELECTOR CALLBACK FUNCTION THAT RETURNS THE STATE OF ANY SLICE.
+	const noOfIcecreams = useSelector((state) => state.icecream.noOfIcecreams);
 	return (
 		<div>
-			<h2>Number of icecreams - </h2>
+			<h2>Number of icecreams - {noOfIcecreams}</h2>
 			<button>Order icecream</button>
 			<button>Restock icecream</button>
 		</div>
