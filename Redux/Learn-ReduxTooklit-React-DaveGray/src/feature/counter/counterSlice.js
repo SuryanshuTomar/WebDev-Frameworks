82641fed4dc9d1013e71e8141ec43eca85c0ff59
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-26 14:48:09 +0530

    Counter slice for the store

---
+++ Redux/Learn-ReduxTooklit-React-DaveGray/src/feature/counter/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	count: 0,
+};
+
+const counterSlice = createSlice({
+	name: "counter",
+	initialState,
+	reducers: {
+		increment: (state, action) => {
+			state.count += 1;
+		},
+		decrement: (state, action) => {
+			state.count -= 1;
+		},
+		reset: (state, action) => {
+			state.count = 0;
+		},
+	},
+	extraReducers: (builder) => {},
+});
+
+export default counterSlice.reducer;
+export const { increment, decrement, reset } = counterSlice.actions;
