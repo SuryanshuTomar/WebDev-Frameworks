96d667d0ceb536114ecd2f51119e247f3ddfdf96
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-30 11:37:52 +0530

    main app

--- TanStackQuery/react-query-II/src/App.jsx
+++ TanStackQuery/react-query-II/src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import Basics from "./components/Basics";
+import Basics from "./components/Basics/Basics";
+import MainQuery from "./components/UseQuery/MainQuery";
+import MainMutation from "./components/UseMutation/MainMutation";
 
 const App = () => {
 	return (
 		<div>
 			<h1>Tanstack React Query</h1>
-			<Basics />
+
+			{/* Basics on useQueryClient, useQuery and useMutation */}
+			{/* <Basics /> */}
+
+			{/* All about useQuery */}
+			<MainQuery />
+
+			{/* All about useQuery */}
+			{/* <MainMutation /> */}
+
+
 		</div>
 	);
 };
