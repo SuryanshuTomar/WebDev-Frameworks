6cd941419a135fe4421402bf5922e37b26ad9674
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-30 11:37:41 +0530

    About use Mutation

---
+++ TanStackQuery/react-query-II/src/components/UseMutation/MainMutation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AboutUseMutation from "./AboutUseMutation";
+
+function MainMutation() {
+	return (
+		<div>
+			<AboutUseMutation />
+
+		</div>
+	);
+}
+
+export default MainMutation;
