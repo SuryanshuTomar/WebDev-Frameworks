413bf0442c54c3e7cb002144acb0bcca537f36a4
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-02 18:19:58 +0530

    Added Code Splitting with lazy and suspense

--- Redux/RTK-Query/src/App.js
+++ Redux/RTK-Query/src/App.js
@@ -1,7 +1,14 @@
-import TodoList from "./features/todos/TodoList";
+import { lazy, Suspense } from "react";
+
+// import TodoList from "./features/todos/TodoList";
+const TodoList = lazy(() => import("../src/features/todos/TodoList.js"));
 
 function App() {
-  return <TodoList />
+	return (
+		<Suspense fallback={<h2>Loading App...</h2>}>
+			<TodoList />
+		</Suspense>
+	);
 }
 
 export default App;
