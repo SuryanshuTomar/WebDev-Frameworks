cfb7142e61f6aa1cf1f206de7930457181b282e3
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-02 18:21:07 +0530

    apiSlice for Todo crud operations using rtkQuery

--- Redux/RTK-Query/src/features/api/apiSlice.js
+++ Redux/RTK-Query/src/features/api/apiSlice.js
@@ -7,6 +7,9 @@ export const apiSlice = createApi({
 	endpoints: (builder) => ({
 		getTodos: builder.query({
 			query: () => "/todos",
+			// transforms the response we are getting from this query and then provide the data to the custom hook returned by the createApi
+			transformResponse: (response) => response.sort((a, b) => b.id - a.id),
+			// set the tag for the current query
 			providesTags: ["Todos"],
 		}),
 		addTodo: builder.mutation({
@@ -17,6 +20,7 @@ export const apiSlice = createApi({
 					body: todo,
 				};
 			},
+			// invalidates cache data for current mutation and trigger query with tag "Todos"
 			invalidatesTags: ["Todos"],
 		}),
 		updateTodo: builder.mutation({
@@ -27,6 +31,7 @@ export const apiSlice = createApi({
 					body: todo,
 				};
 			},
+			// invalidates cache data for current mutation and trigger query with tag "Todos"
 			invalidatesTags: ["Todos"],
 		}),
 		deleteTodo: builder.mutation({
@@ -35,6 +40,7 @@ export const apiSlice = createApi({
 				method: "DELETE",
 				body: id,
 			}),
+			// invalidates cache data for current mutation and trigger query with tag "Todos"
 			invalidatesTags: ["Todos"],
 		}),
 	}),
