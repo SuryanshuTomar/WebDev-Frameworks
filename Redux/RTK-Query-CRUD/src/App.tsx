2562aad9831c6e6d391a00810722d8fb6fc0c665
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-09 15:51:36 +0530

    App components that displays contact from queries

--- Redux/RTK-Query-CRUD/src/App.tsx
+++ Redux/RTK-Query-CRUD/src/App.tsx
@@ -1,6 +1,38 @@
+import { useGetContactsQuery } from "./api/contactsApi";
+import ContactDetails from "./components/ContactDetails";
+import { Contact } from "./models/contact.model";
+
 function App() {
-  return (
-    <div>App</div>
-  )
+	const {
+		data: contacts,
+		isSuccess,
+		isError,
+		isLoading,
+		isFetching,
+		error,
+	} = useGetContactsQuery();
+
+	if (isLoading) return <h2>Loading...</h2>;
+	else if (isFetching) return <h2>Fetching Contacts...</h2>;
+	else if (isError)
+		return <h2>Something went wrong...{JSON.stringify(error)}</h2>;
+	else if (isSuccess)
+		return (
+			<div>
+				{contacts.map((contact: Contact) => (
+					<div key={contact.id}>
+						<h2>Contact: {contact.id}</h2>
+						<span>{contact.name}</span> &nbsp;
+						<span>{contact.email}</span>
+						<span>
+							<ContactDetails id={contact.id} />
+						</span>
+            <hr />
+					</div>
+				))}
+			</div>
+		);
+
+	return <div>No Data Received</div>;
 }
-export default App
+export default App;
