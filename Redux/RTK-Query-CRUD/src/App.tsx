0a3fa84511492c6d06b7a99dab2d802aad025d5b
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-09 19:52:26 +0530

    added ContactForm and uuid

--- Redux/RTK-Query-CRUD/src/App.tsx
+++ Redux/RTK-Query-CRUD/src/App.tsx
@@ -1,6 +1,8 @@
 import { useGetContactsQuery } from "./api/contactsApi";
+import AddContactForm from "./components/AddContactForm";
 import ContactDetails from "./components/ContactDetails";
 import { Contact } from "./models/contact.model";
+import { v4 as uuid } from "uuid";
 
 function App() {
 	const {
@@ -18,18 +20,25 @@ function App() {
 		return <h2>Something went wrong...{JSON.stringify(error)}</h2>;
 	else if (isSuccess)
 		return (
-			<div>
-				{contacts.map((contact: Contact) => (
-					<div key={contact.id}>
-						<h2>Contact: {contact.id}</h2>
-						<span>{contact.name}</span> &nbsp;
-						<span>{contact.email}</span>
-						<span>
-							<ContactDetails id={contact.id} />
-						</span>
-            <hr />
-					</div>
-				))}
+			<div className="container">
+				<div>
+					<h2>Contacts</h2>
+					{contacts.map((contact: Contact) => (
+						<div key={contact.id}>
+							<h3>Contact: {contact.id}</h3>
+							<span>{contact.name}</span> &nbsp;
+							<span>{contact.email}</span>
+							<span>
+								<ContactDetails id={contact.id} />
+							</span>
+							<hr />
+						</div>
+					))}
+				</div>
+				<div>
+					<h2>Contact Form</h2>
+					<AddContactForm id={uuid()} />
+				</div>
 			</div>
 		);
 
