d5f2d566051ee256ddef76d2f1e416f3f3cf78e9
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-09 19:53:32 +0530

    added delete feature using contactApi

--- Redux/RTK-Query-CRUD/src/components/ContactDetails.tsx
+++ Redux/RTK-Query-CRUD/src/components/ContactDetails.tsx
@@ -1,7 +1,22 @@
-import { useGetContactQuery } from "../api/contactsApi";
+import {
+	useDeleteContactMutation,
+	useGetContactQuery,
+} from "../api/contactsApi";
 
 const ContactDetails = ({ id }: { id: string }) => {
+	const [deleteContact] = useDeleteContactMutation();
 	const { data: contact } = useGetContactQuery(id);
-	return <pre>{JSON.stringify(contact, undefined, 2)}</pre>;
+
+	const handleDelete = async (event: React.MouseEvent<HTMLButtonElement>) => {
+		await deleteContact(id);
+	};
+
+	return (
+		<div>
+			<pre>{JSON.stringify(contact, undefined, 2)}</pre>
+			<button onClick={handleDelete}>Delete Contact</button>
+			<button>Edit Contact</button>
+		</div>
+	);
 };
 export default ContactDetails;
