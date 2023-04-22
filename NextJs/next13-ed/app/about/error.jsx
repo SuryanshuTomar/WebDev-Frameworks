77979f7c72f8d30cf92bf30140c2769961ae7334
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-04-22 13:38:28 +0530

    Additional info on Error Boundary for about page

--- NextJs/next13-ed/app/about/error.jsx
+++ NextJs/next13-ed/app/about/error.jsx
@@ -5,12 +5,13 @@ import { useEffect } from "react";
 function Error({ error, reset }) {
 	useEffect(() => {
 		// Log the error to an error reporting service
-		console.error(error);
+		console.info(error);
 	}, [error]);
 
 	return (
 		<div>
-			<h2>Something went wrong!</h2>
+			<h2>Something went wrong! : </h2>
+			<p>{error.message}</p>
 			<button onClick={() => reset()}>Reset error boundary</button>
 		</div>
 	);
