6bd1c6d155a4e8b4a7ea064056c674973ecc25f8
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-04-22 17:17:47 +0530

    Home Page

--- NextJs/next13-ed/app/page.jsx
+++ NextJs/next13-ed/app/page.jsx
@@ -1,9 +1,24 @@
+// imports
+import Movie from "./Movie";
 import styles from "./page.module.css";
 
-export default function Home() {
+// https://api.themoviedb.org/3/movie/popular
+export default async function Home() {
+	const data = await fetch(
+		`https://api.themoviedb.org/3/movie/popular/?api_key=${process.env.API_KEY}`
+	);
+	const res = await data.json();
+	console.log(res);
+
 	return (
 		<main>
-			<h1>Hello Next 13</h1>
+			<h1 className={"text-xl"}>Popular Movies</h1>
+			<br />
+			<div className="grid gap-16 grid-cols-fluid">
+				{res.results.map((movie) => (
+					<Movie key={movie.id} movie={movie} />
+				))}
+			</div>
 		</main>
 	);
 }
