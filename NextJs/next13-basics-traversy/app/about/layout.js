f070304cd227b04ddf1e9fe44c973cb9723159d3
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-04-24 14:51:17 +0530

    About Layout

---
+++ NextJs/next13-basics-traversy/app/about/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+	title: "About Next Matrix",
+	description: "This page holds information about Next Matrix",
+};
+
+const AboutLayout = ({ children }) => {
+	return (
+		<div>
+			{/* <h1 className="m-2">About Layout</h1> */}
+			{children}
+		</div>
+	);
+};
+export default AboutLayout;
