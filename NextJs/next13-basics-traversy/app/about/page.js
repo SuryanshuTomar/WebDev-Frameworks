2d5fa3e52864c7f55631ef907b0eadd2c13fa096
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-04-24 14:52:06 +0530

    About Page

---
+++ NextJs/next13-basics-traversy/app/about/page.js
@@ -0,0 +1,22 @@
+const AboutPage = () => {
+	return (
+		<div>
+			<h1>About Next Matrix</h1>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, a
+				eligendi? Provident aliquam iusto repellat aspernatur libero
+				repudiandae velit minus, laudantium, amet ab itaque consectetur
+				placeat dolorum eligendi. Doloribus temporibus culpa quidem
+				laboriosam excepturi nostrum! Placeat tempora asperiores ut hic, non
+				dolor mollitia porro quas labore esse sequi, similique nesciunt,
+				facere aliquam? Magnam in facere doloremque laudantium dicta minima
+				laborum pariatur unde beatae maxime hic corporis architecto expedita
+				eius ratione earum iste, obcaecati quis quisquam odit aspernatur
+				atque corrupti. Suscipit ex illo inventore tempore placeat aut
+				voluptas id alias quaerat praesentium omnis, possimus, ipsum
+				laudantium fugit, minus consequuntur? Nostrum, deleniti?
+			</p>
+		</div>
+	);
+};
+export default AboutPage;
