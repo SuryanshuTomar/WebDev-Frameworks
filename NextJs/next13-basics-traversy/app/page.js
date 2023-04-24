55211e2dde72340cab2e2b82dd6ac97fc51e3aad
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-04-24 14:51:58 +0530

    HomePage

--- NextJs/next13-basics-traversy/app/page.js
+++ NextJs/next13-basics-traversy/app/page.js
@@ -1,4 +1,21 @@
+import Link from "next/link";
+
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			<h1>Welcome to Next Matrix</h1>
+			<ul>
+				<li className="mx-2">
+					<Link href="/">Home</Link>
+				</li>
+				<li className="mx-2">
+					<Link href="/about">About</Link>
+				</li>
+				<li className="mx-2">
+					<Link href="/about/team">Team</Link>
+				</li>
+			</ul>
+		</div>
+	);
 };
 export default HomePage;
