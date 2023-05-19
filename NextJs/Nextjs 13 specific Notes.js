aed8c73bd3ccb21c84b231efca7d6a1876709e33
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-19 23:28:42 +0530

    Next 13 specific notes

---
+++ NextJs/Nextjs 13 specific Notes.js
@@ -0,0 +1,4 @@
+// => NEXT JS INTRO AND BASICS -
+
+// 1. By Default, Every component in NextJs13 onwards is a React Server Component. That means these components are rendered server side and not on the client side.
+// 2. If we want to render any component client side then we can use, "use client" directive can be used at the top of any component to make a nextjs13 component a React Client Component.
