8d9fe9b3b07c964fb7501e8f5d7561d16e99bb6a
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-05-20 17:09:01 +0530

    Notes Metadata

--- NextJs/Nextjs 13 specific Notes.js
+++ NextJs/Nextjs 13 specific Notes.js
@@ -9,7 +9,8 @@
 //       ii. Layout for that particular route in the app using layout.js file
 //       iii. Loading UI component while the page is loading using the loading.js file.
 
-// The Next JS API -
+// -----------------------------------------------------------------------------------------
+// => The Next JS API -
 // - There are two way in Next js using which we can create API Endpoints -
 // 1. Create a route.js file for each route directory just like we are creating page.js file for each route directory. But, note that each directory can either have page.js file or route.js file. We can not put both, as nextjs will not be able to render both api route and app route.
 // 2. Create an api folder inside the app directory, And Inside that directory, create a directory for which we want to create an api route just like we are creating for the app file based routing. And inside that directory create a new route.js file and write your route handlers. (Recommended Method for Creating API)
@@ -27,4 +28,44 @@
 // Example Syntax -
 // export async function GET(request) {
 // 	return new Response("Hello Nextjs!!");
+// }
+
+// -----------------------------------------------------------------------------------------
+// => MetaData -
+
+// - We can define Metadata in 2 ways :
+// 1. Static Metadata
+// 2. Dynamic Metadata
+
+// 1. Static Metadata Example -
+// export const metadata = {
+// 	title: "HomePage",
+// };
+
+// Output ->
+/*
+<head>
+   <title>HomePage</title>
+</head>
+*/
+
+// export default function Page() {
+// 	return <h1>Normal Next.js Homepage</h1>;
+// }
+
+// 2. Dynamic Metadata Example -
+// export async function generateMetadata({ params, searchParams }) {
+// 	const product = await getProduct(params.id);
+// 	return { title: product.title };
+// }
+
+// Output ->
+/*
+<head>
+   <title>My Unique Product</title>
+</head>
+*/
+
+// export default function Page() {
+// 	return <h1>Normal Next.js Homepage</h1>;
 // }
